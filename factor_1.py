f371d00ff4db20e74c2248fc1beb52fcc3660fb9
empresa <venditto04@gmail.com>
2025-07-24 16:30:42 -0300

    ultimas alteracoes

---
+++ factor_1.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import quantstats as qs
+import yfinance as yf
+import bt as bt
+import matplotlib.ticker as mtick
+import matplotlib.pyplot as plt
+plt.style.use("dark_background")
+
+dados_empresas = pd.read_excel("statusinvest-busca-avancada.xlsx")
+
+
