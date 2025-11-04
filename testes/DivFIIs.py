6671f43677a3dd5c71e8d117221379371c360573
empresa <venditto04@gmail.com>
2025-11-04 17:51:19 -0300

    new features

---
+++ testes/DivFIIs.py
@@ -0,0 +1,9 @@
+#python -m pip install fundamentus
+import fundamentus
+
+detalhes_fii = fundamentus.get_detalhes_fii('MXRF11')
+
+print(f"Informações para o FII MXRF11:")
+print(f"Preço: R$ {detalhes_fii.iloc[0]['cotacao']}")
+print(f"Dividend Yield (12M): {detalhes_fii.iloc[0]['dy']}%")
+print(f"P/VP: {detalhes_fii.iloc[0]['p_v']}")
